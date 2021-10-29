c0e782a40d012c5bf3f5408e66c9df91b3066245
Fernanda <Fernanda@LAPMARIBEL>
2021-10-28 23:56:06 -0500

    Agregar archivos de proyecto.

---
+++ PDSMS2Tirira/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDSMS2Tirira
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string usuario, string clave)
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            //almacenar los datos en variables
+            string usuario = txtUsuario.Text;
+            string clave = txtClave.Text;
+
+            //Navegacion a la ventana dos
+
+            await Navigation.PushAsync(new ViewDos(usuario, clave));
+
+
+        }
+    }
+}
